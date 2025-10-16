def39a4d80a2b249c1fe885221246633c980de74
mohammednazim007 <mohammednazim3629@gmail.com>
2025-10-16 17:41:39 +0600

    feat(user): add social features to user model and interface
    Add friend relationships, friend requests, blocking functionality, and activity tracking to the user model. Includes schema updates in both interface and model files to support these new social features.

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,3 +1,19 @@
+// import mongoose, { Schema } from "mongoose";
+// import { IUser } from "./user.interface";
+
+// const userSchema = new Schema<IUser>(
+//   {
+//     name: { type: String, required: true },
+//     email: { type: String, unique: true, required: true },
+//     password: { type: String, required: true },
+//     avatar: { type: String, default: null },
+//   },
+//   { timestamps: true }
+// );
+
+// const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+// export default User;
 import mongoose, { Schema } from "mongoose";
 import { IUser } from "./user.interface";
 
@@ -7,6 +23,13 @@ const userSchema = new Schema<IUser>(
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     avatar: { type: String, default: null },
+
+    friends: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    friendRequests: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    sentRequests: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    blockedUsers: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+
+    lastActive: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
