0c232e8770c9d777fc7777da59e2ae9b4135d52c
mohammednazim007 <mohammednazim3629@gmail.com>
2025-11-15 21:29:44 +0600

    feat: implement user validation schemas and error handling utilities
    - Added Zod validation schemas for user registration and login.
    - Implemented utility functions for generating OTPs and handling Zod errors.
    - Created JWT verification and refresh token functionality.
    - Established socket.io integration for real-time notifications and user presence.
    - Configured cookie options for access and refresh tokens.
    - Set up Vercel API handler with database connection management.
    - Updated project structure and added necessary TypeScript definitions.

--- src/cloudinary/upload.ts
+++ src/cloudinary/upload.ts
@@ -1,27 +1,3 @@
-// import multer from "multer";
-// import { CloudinaryStorage } from "multer-storage-cloudinary";
-// import cloudinary from "@/cloudinary/cloudinary";
-
-// const storage = new CloudinaryStorage({
-//   cloudinary,
-//   params: {
-//     folder: "uploads", // Cloudinary folder
-//     allowed_formats: ["jpg", "png", "jpeg", "webp"],
-//     resource_type: "auto",
-//     transformation: [
-//       { width: 300, height: 300, crop: "limit" },
-//       { quality: "auto" },
-//       { fetch_format: "auto" },
-//     ],
-//   } as {
-//     folder: string;
-//     allowed_formats: string[];
-//     resource_type: string;
-//   },
-// });
-
-// export const upload = multer({ storage });
-
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "@/cloudinary/cloudinary";
 import multer, { FileFilterCallback } from "multer";
